0294252e6d5443fc6ed67eeacfc8afe14c78e746
Rhys Streefland <rhys@streefland.co.uk>
2020-05-25 12:37:53 +0100

    chore: more renaming

--- Source/SPAboutController.m
+++ Source/SPAboutController.m
@@ -65,7 +65,7 @@ static NSString *SPShortVersionHashKey = @"SPVersionShortHash";
 	BOOL isSnapshotBuild = matchRange.location != NSNotFound;
 	
 	// Set the application name, but only include the major version if this is not a nightly build.
-	[appNameVersionTextField setStringValue:isSnapshotBuild ? @"Sequel Pro" : [NSString stringWithFormat:@"Sequel Pro %@", version]];
+	[appNameVersionTextField setStringValue:isSnapshotBuild ? @"rSequel Pro" : [NSString stringWithFormat:@"rSequel Pro %@", version]];
 
 	[self _setVersionLabel:isSnapshotBuild];
 	
